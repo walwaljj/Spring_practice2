926e75347323dd94fb6ee5cd9fbe7fb397527a83
Shjung <wjdtngus9@gmail.com>
2022-12-27 22:24:58 +0900

    .

---
+++ src/main/java/com/example/firstproject/Article/Article.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.Article;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity // DB가 해당 객체를 인식하게함.
+public class Article {
+
+    @GeneratedValue// 1,2,3,,, 등 순서를 자동을 생성해줌,
+    @Id//같은 이름과 내용을 구분하기 위함.
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.Article.Article;
 import com.example.firstproject.DTO.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,11 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
+
+        // 1.DTO 변환 -> Entity 로 .
+        Article article = form.toEntity();
+
+        // 2.Repository 에게 Entity 를 DB에 저장하게 하기.
         return "";
     }
 }
